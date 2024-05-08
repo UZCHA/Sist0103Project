2776e6a4058fa0b593612c4be47b4ed0b86b40fc
uz <xaxaxa7@naver.com>
2024-05-08 17:31:23 +0900

    day0508

---
+++ sts3work/SpringDbTest/src/main/java/spring/mysql/coffee/MyShopDaoInter.java
@@ -0,0 +1,10 @@
+package spring.mysql.coffee;
+
+import java.util.List;
+
+public interface MyShopDaoInter {
+
+	List<MyShopDto> getAllList();
+	
+	
+}
---
+++ sts3work/SpringDbTest/src/main/java/spring/mysql/coffee/MyShopDto.java
@@ -0,0 +1,42 @@
+package spring.mysql.coffee;
+
+public class MyShopDto {
+	private String num;
+	private String sangpum;
+	private String color;
+	private String price;
+	private String imagename;
+	
+	public String getNum() {
+		return num;
+	}
+	public void setNum(String num) {
+		this.num = num;
+	}
+	public String getSangpum() {
+		return sangpum;
+	}
+	public void setSangpum(String sangpum) {
+		this.sangpum = sangpum;
+	}
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+	public String getPrice() {
+		return price;
+	}
+	public void setPrice(String price) {
+		this.price = price;
+	}
+	public String getImagename() {
+		return imagename;
+	}
+	public void setImagename(String imagename) {
+		this.imagename = imagename;
+	}
+
+
+}
